51490750db565f94b4d6f6970042a27c58ff7e4f
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-01-05 09:24:34 +0800

    12

--- myapp/views/__init__.py
+++ myapp/views/__init__.py
@@ -14,8 +14,8 @@ Created on Mon Apr 18 09:00:06 2022
 
 #from .views_monthlyAlterStuff import *
 from .views_stock6 import *  #要有「點」
-
-
+from .views_stockPERseg import *  #要有「點」
+from .views_tseotc_id import *  #要有「點」
 """
 from .views_stock6DB import *  #要有「點」
 
---
+++ myapp/views/views_stockPERseg.py
@@ -0,0 +1,5 @@
+from module_PERseg import PERsegx
+from module_PERseg import PERsegxMain
+from module_PERseg import PERsegxDB
+from module import func2
+from .views_tseotc_id import *
