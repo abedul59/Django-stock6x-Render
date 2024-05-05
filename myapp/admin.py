8fc8910d510cce2b5feca759778d763966eeb15f
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-05-05 20:39:41 +0800

    5/5 mongodb works add 3

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from myapp.models import Stock6Sign202404
 from myapp.models import NewsUnit
 from myapp.models import Person
+from myapp import models
 
 admin.site.register(NewsUnit)
 admin.site.register(Person)
-admin.site.register(Stock6Sign202404)
+admin.site.register(models.MacroWaveA)
+admin.site.register(models.MacroWaveB)
+admin.site.register(models.MacroWaveC)
+admin.site.register(models.USBondYieldDB)
+
+
+admin.site.register(models.Stock6Sign202404)
+admin.site.register(models.StockPERseg202404)
