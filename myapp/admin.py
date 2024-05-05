86383e5a5140e46d375d8ed6f2a6675a38e244fc
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-05-05 08:24:51 +0800

    5/5 add MongoDB 2

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from myapp.models import Stock6Sign202404
+from myapp.models import NewsUnit
+from myapp.models import Person
 
+admin.site.register(NewsUnit)
+admin.site.register(Person)
 admin.site.register(Stock6Sign202404)
--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,48 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+
+    
+class Person(User):
+    cName = models.CharField(max_length=20, default='')
+    cCellphone = models.CharField(max_length=10, default='')
+
+    class Meta:
+        permissions = (
+            ("Can_enter_stock6", "Can enter stock6"),
+            ("Can_enter_stock6 DB", "Can enter stock6 DB"),
+            ("Can_enter_stockPERseg", "Can enter stockPERseg"),
+            ("Can_enter_stockPERseg DB", "Can enter stockPERseg DB"),
+            ("Can_enter_All", "Can enter All"), 
+            ("Can_enter_PaidUsersOnly", "Can enter PaidUsersOnly"),  
+            ("Can_enter_AdminOnly", "Can enter AdminOnly"),
+            ("Can_enter_VIPsOnly", "Can enter VIPsOnly"),                
+            ("Can_enter_stockKn", "Can enter stockKn"),
+            ("Can_enter_usersmain_test168", "Can enter usersmain_test168"),
+
+        )
+    def __str__(self):
+        return self.cName
+
+class NewsUnit(models.Model):
+    catego = models.CharField(max_length=10, default='')
+    nickname = models.CharField(max_length=20, default='')
+    title = models.CharField(max_length=50, default='')
+    message = models.TextField(max_length=100, default='')
+    pubtime = models.DateTimeField(auto_now=True)
+    enabled = models.BooleanField(default=False)
+    press = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+
 class Stock6Sign202404(models.Model):  
     cStockID = models.CharField(max_length=5, default='')
     cStockName = models.CharField(max_length=5, default='')
-    
-    
     cNewestSeason = models.CharField(max_length=15, default='')
     cNewestRev = models.CharField(max_length=15, default='')
-    
     cSign1 = models.CharField(max_length=10, default='')
     cSign2 = models.CharField(max_length=10, default='')
     cSign3 = models.CharField(max_length=10, default='')
@@ -30,12 +64,6 @@ class Stock6Sign202404(models.Model):
     sCore2306= models.CharField(max_length=10, default='')
     sCore2305= models.CharField(max_length=10, default='')
     sCore2304= models.CharField(max_length=10, default='')
-
-
-
-
-
-
 
     pubtime = models.DateTimeField(auto_now=True)
     enabled = models.BooleanField(default=False)
